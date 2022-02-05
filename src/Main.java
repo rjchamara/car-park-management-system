fdad6c7750e952c16abd2d63d6404ab4c5d04901
Amalka Lakshan <91656103+LakshanUS@users.noreply.github.com>
2022-02-05 21:35:26 +0530

    I added arraylist set for check availability of parks. and parking slots

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,16 @@
+
+import java.util.ArrayList;
 public class Main {
 
+
     public static void main(String[] args) {
+        /*ArrayList set for check parks and slots availability*/
+        ArrayList<String> checktechslot = new ArrayList<>();
+        ArrayList<String> checkmedislot = new ArrayList<>();
+        ArrayList<String> checkartsslot = new ArrayList<>();
+        ArrayList<String> checktechpark = new ArrayList<>();
+        ArrayList<String> checkmedipark = new ArrayList<>();
+        ArrayList<String> checkartspark = new ArrayList<>();
 	
     }
 }
