b88fd1d4b9b5bd2d0a09e109d6090e6d4f718fd2
Deshzz23 <2019t00386@stu.cmb.ac.lk>
2022-02-05 22:26:56 +0530

    I added a as4 parking slot

--- src/Main.java
+++ src/Main.java
@@ -131,6 +131,14 @@ public class Main {
         checkartsslot.add(artsslots.slotid);
         checkartspark.add(artsslots.slotid);
 
+        artsslots.slotid = "as4";
+        artsslots.slotgps = "6.9364° N, 79.8487° E";
+        artsslots.slottype = "standard";
+        artsslots.slotimformation = "----";
+        artsslots.addslot();
+        checkartsslot.add(artsslots.slotid);
+        checkartspark.add(artsslots.slotid);
+
 	
     }
 }
