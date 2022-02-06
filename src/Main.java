8b3a482ac4ecde5a249f253b4c254f66b84b0e67
Amalka Lakshan <91656103+LakshanUS@users.noreply.github.com>
2022-02-06 10:16:36 +0530

    i added tl2 parking slot

--- src/Main.java
+++ src/Main.java
@@ -73,6 +73,14 @@ public class Main {
         checktechslot.add(techslots.slotid);
         checktechpark.add(techslots.slotid);
 
+        techslots.slotid = "tl2";
+        techslots.slotgps = "6.9364° N, 79.8487° E";
+        techslots.slottype = "long vehicle";
+        techslots.slotimformation = "length: 6m, width: 3m";
+        techslots.addslot();
+        checktechslot.add(techslots.slotid);
+        checktechpark.add(techslots.slotid);
+
 
         CarPark medi = new CarPark(); //medicine faculty car park
 
